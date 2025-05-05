f0f0db09d061e0cbce1dca352c0c8f1aca70ac6f
a <a@gmail.com>
2025-05-05 20:55:31 -0300

    reclamao

---
+++ FRONTEND/TODOapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
